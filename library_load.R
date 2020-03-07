10cc2a904f52e78e5e24eb44c7c58f44c3c022f5
Xander Posner <xander@berkeley.edu>
2020-03-06 17:09:34 -0800

    libraries

---
+++ library_load.R
@@ -0,0 +1,26 @@
+# The first time you open this R script (.R) document, 
+# RStudio should prompt you to install all below packages if they
+# are not currently installed on your machine.
+
+# To test if the packages installed correctly, do the following:
+# 1. open the R markdown (.Rmd) file named "test.Rmd"
+# 2. click Knit above and then click Knit to PDF.
+# 3. if a knitted PDF opens automatically, you're done!
+
+# EACH TIME you open RStudio thereafter, do the following:
+# 1. open this document again
+# 2. press control+A (PC) or command+A (Mac) to select all text
+# 3. click Run to load all libraries
+
+library(rmarkdown)
+library(knitr)
+library(epiR)
+library(stats)
+library(tidyverse)
+library(readr)
+library(readxl)
+library(epitools)
+library(exact2x2)
+library(DescTools)
+library(MESS)
+library(pwr)
