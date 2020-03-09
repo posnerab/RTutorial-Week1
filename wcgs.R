7c1cf915badcf7278ffa76cbd82e9f4732fdcaf9
Xander Posner <xander@berkeley.edu>
2020-03-08 18:12:34 -0700

    update for tables

--- wcgs.R
+++ wcgs.R
@@ -1,8 +1,2 @@
-# library(tidyverse)
-# library(readr)
-# library(epitools)
-# library(markdown)
-
 wcgs <- read_csv("wcgs_data.csv", col_names = TRUE)
 wcgs_new <- read_csv("wcgs_data_new.csv", col_names = TRUE)
-levels(wcgs_new$dibpat0) <- c("A", "B")
